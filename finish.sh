79d88f861574ffb989cbc6b08ff05f2b620bbbe8
UladKasach <nappa300@gmail.com>
2017-07-15 10:08:56 -0400

    upgrade sol to #16 w/ logging

--- finish.sh
+++ finish.sh
@@ -1,2 +1,8 @@
 echo "Running finish $1 times with claimant $2"
-for i in {1..$1}; do node scraping_finish.js $2; done
+for i in {1..$1}; do 
+    start=`date +%s`
+    node scraping_finish.js $2
+    end=`date +%s`
+    runtime=$((end-start))
+    echo $runtime >> runtimes.log
+done
