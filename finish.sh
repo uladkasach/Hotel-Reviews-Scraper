12fe88a7bec0274942af2ccfab504f293166e853
UladKasach <nappa300@gmail.com>
2017-07-15 10:23:25 -0400

    Fix sequence for #16

--- finish.sh
+++ finish.sh
@@ -1,8 +1,11 @@
 echo "Running finish $1 times with claimant $2"
-for i in {1..$1}; do 
+for i in `seq $1`; do 
     start=`date +%s`
     node scraping_finish.js $2
     end=`date +%s`
     runtime=$((end-start))
     echo "run for time $i, for claimant $2, took "$runtime >> runtimes.log
 done
+
+# touch /var/www/git/Hotel-Reviews-Scraper/runtimes.log
+# sudo tail -fn 1000 /var/www/git/Hotel-Reviews-Scraper/runtimes.log
